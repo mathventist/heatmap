b5dfd1ef9e9aa31cfd0c0244b75755fbb897b63d
Alan Kydd <mathventist@gmail.com>
2021-12-21 14:33:33 -0700

    Added validation and returned errors

--- heatmap.go
+++ heatmap.go
@@ -1,6 +1,8 @@
 package heatmap
 
 import (
+	"errors"
+	"fmt"
 	"image"
 	"image/color"
 	"image/png"
@@ -11,34 +13,33 @@ import (
 type heatMap struct {
 	image     *image.RGBA
 	blockSize int
-	baseColor color.RGBA
 	width     int
 	height    int
 }
-
-var (
-	Black = color.RGBA{0, 0, 0, 255}
-	//Red     = color.RGBA{255, 0, 0, 255}
-	//Green   = color.RGBA{0, 255, 0, 255}
-	//Blue    = color.RGBA{0, 0, 255, 255}
-	//Yellow  = color.RGBA{255, 255, 0, 255}
-	//Cyan    = color.RGBA{0, 255, 255, 255}
-	//Magenta = color.RGBA{255, 0, 255, 255}
-)
 
 func newHeatMap(xDim, yDim int, blockSize int) *heatMap {
 	return &heatMap{
 		image:     image.NewRGBA(image.Rect(0, 0, blockSize*xDim, blockSize*yDim)),
 		blockSize: blockSize,
-		baseColor: Black,
 		width:     xDim,
 		height:    yDim,
 	}
 }
 
-// AddBlock shades the block at (x, y), colored according to the score
-func (h *heatMap) AddBlock(x, y int, score float32) {
-	col := floatToRGBA(score, h.baseColor)
+func (h *heatMap) addBlock(x, y int, score float32) error {
+	if x < 0 || x > h.width-1 || y < 0 || y > h.height-1 {
+		errorString := fmt.Sprintf("heatmap coordinate (%v, %v) is out of bounds", x, y)
+		return errors.New(errorString)
+	}
+
+	if score < 0 {
+		return errors.New("score must be greater or equal to 0")
+	}
+
+	col, err := floatToGreyscale(score)
+	if err != nil {
+		return err
+	}
 
 	for x1 := x * h.blockSize; x1 < (x+1)*h.blockSize; x1++ {
 		// Flip image around the x axis so that the y coordinate increases upwards, not downwards.
@@ -46,56 +47,60 @@ func (h *heatMap) AddBlock(x, y int, score float32) {
 			h.image.Set(x1, y1, col)
 		}
 	}
+
+	return nil
 }
 
 // DrawHeatMap produces a heatmap image PNG with filenam "name.png"
-func DrawHeatMap(data [][]float32, blockSize int, name string) {
+func DrawHeatMap(data [][]float32, blockSize int, name string) error {
+	if blockSize < 1 {
+		return errors.New("heatmap blocksize must be positive")
+	}
+
+	if name == "" {
+		return errors.New("heatmap filename cannot be blank")
+	}
+
+	if len(data) == 0 || len(data[0]) == 0 {
+		return errors.New("heatmap is missing data in one or more dimensions")
+	}
+
 	h := newHeatMap(len(data), len(data[0]), blockSize)
 
 	for i, d := range data {
 		for j, s := range d {
-			h.AddBlock(i, j, s)
+			if err := h.addBlock(i, j, s); err != nil {
+				return err
+			}
 		}
 	}
 
 	f, err := os.Create(name + ".png")
 	if err != nil {
-		panic(err)
+		return err
 	}
+
 	defer f.Close()
-	png.Encode(f, h.image)
+
+	if err = png.Encode(f, h.image); err != nil {
+		return err
+	}
+
+	return nil
 }
 
-// TODO: implement this properly. For now it only operates from black to white.
-// Combine a float between 0 and 1, and a base rgba, to create a new rgba, where the
-// float is treated as the lightness component of the HSV color model.
-func floatToRGBA(f float32, base color.RGBA) color.RGBA {
-	v := f * 255
+// Convert float [0, 1] to greyscale, [#000000, #FFFFFF]
+func floatToGreyscale(score float32) (color.RGBA, error) {
+	if score < 0 {
+		return color.RGBA{}, errors.New("score must be greater or equal to 0")
+	}
+
+	v := score * 255
 
 	return color.RGBA{
 		R: uint8(math.Round(float64(v))),
 		G: uint8(math.Round(float64(v))),
 		B: uint8(math.Round(float64(v))),
 		A: 255,
-	}
-	// Step 1: determine the hue from the base color
-	//hue := rgbaToHue(base)
-
-	//// Step 2: determine the saturation
-	//sat := rgbaToSaturation(base)
-
-	//// Step 3: set the lightness
-	//light := f
-
-	//// Step 4: recombine to a new rgba model
-	//return HSVToRBGA(hue, sat, light)
+	}, nil
 }
-
-//func rgbaToHue(c color.RGBA) float32 {
-//}
-//
-//func rgbaToSaturation(c color.RGBA) float32 {
-//}
-//
-//func HSVToRBGA(h, s, l float32) color.RGBA {
-//}
